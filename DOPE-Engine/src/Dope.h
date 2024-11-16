b6b96e89756106b9104ed80f6cbcc3c4707bef90
Ishan Singh <Is26092003@gmail.com>
2024-11-16 23:20:49 +0530

    Logging system

--- DOPE-Engine/src/Dope.h
+++ DOPE-Engine/src/Dope.h
@@ -2,7 +2,8 @@
 
 //for use by DOPE applications
 #include<stdio.h>
-#include <Dope/Application.h>
+#include "Dope/Application.h"
+#include "Dope/Log.h"
 
 //---Entry Point---------
 #include "Dope/EntryPoint.h"
--- DOPE-Engine/src/Dope/EntryPoint.h
+++ DOPE-Engine/src/Dope/EntryPoint.h
@@ -1,11 +1,16 @@
 #pragma once
-#include "Application.h"
+#include "Dope.h"
+
 
 #ifdef D_PLATFORM_WINDOWS
 
 extern Dope::Application* Dope::CreateApplication();
 
 int main(int argc, char** argv) {
+	Dope::Log::Init();
+	D_CORE_WARN("Initialized Log");
+	D_INFO("Hello");
+
 	printf("Dope Engine");
 	auto app = Dope::CreateApplication();
 	app->Run();
